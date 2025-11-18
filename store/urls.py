1691d693dd6fc5a858f845ee3edc832d173d1a77
darylepanaglimalspu <0324-0917@lspu.edu.ph>
2025-11-18 19:32:12 +0800

    Added Login, Logout, Navigation bar

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('', views.product_list, name='product-list'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 from .models import Product
 
 def product_list(request):
-
     products = Product.objects.all()
-
     return render(request, 'store/product_list.html', {'products': products})
+
+def register_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('store:product-list')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'store/register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('store:product-list')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request,'store/Login.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('store:product-list')
